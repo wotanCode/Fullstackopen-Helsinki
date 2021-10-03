901f22791d45c37b474629d218c691142383a686
Pedro Yanez <pedro_y91@outlook.com>
2021-10-03 20:25:35 -0300

    se borra el contenido del css

--- part01/1.6-1.14/unicafe/src/App.js
+++ part01/1.6-1.14/unicafe/src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  
-  return (
-    <div className="App">
-      hola mundillo
-    </div>
-  );
+  return <div className="App">hola mundillo</div>;
 }
 
 export default App;
