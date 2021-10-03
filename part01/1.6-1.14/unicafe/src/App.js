39708f3b10d297d4b65b018fb9025c599f5813fd
Pedro Yanez <pedro_y91@outlook.com>
2021-10-03 20:49:33 -0300

    paso 1 listo

--- part01/1.6-1.14/unicafe/src/App.js
+++ part01/1.6-1.14/unicafe/src/App.js
@@ -1,7 +1,38 @@
+import React, {useState} from "react"
 import "./App.css";
 
 function App() {
-  return <div className="App">hola mundillo</div>;
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    console.log("good +1")
+    setGood(good+1)
+  }
+
+  const handleNeutral = () => {
+    console.log("Neutral +1")
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    console.log("Bad +1")
+    setBad(bad+1)
+  }
+
+  return (
+    <div className="App">
+      <h1>Give Feedback</h1>
+      <button onClick={() => handleGood()}>Good</button>
+      <button onClick={() => handleNeutral()}>Neutral</button>
+      <button onClick={() => handleBad()}>Bad</button>
+      <h2>Statitics</h2>
+      <p><b>good</b> {good}</p>
+      <p><b>neutral</b> {neutral}</p>
+      <p><b>bad</b> {bad}</p>
+    </div>
+  );
 }
 
 export default App;
