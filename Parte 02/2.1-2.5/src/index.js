3959a58c39862c652adb8ecf1ead346be07eca04
Pedro Yanez <pedro_y91@outlook.com>
2021-10-11 20:58:54 -0300

    preparando terreno para la seccion 2

---
+++ Parte 02/2.1-2.5/src/index.js
@@ -0,0 +1,81 @@
+/*
+Basado en la seccion
+https://fullstackopen.com/es/part1/introduccion_a_react
+https://fullstackopen.com/es/part1/java_script
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Componente para el Header
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+//Componente para el contenido.
+const Content = (props) => {
+  return (
+    <>
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
+    </>
+  );
+};
+
+//Part
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts.name} {props.parts.exercises}
+      </p>
+    </>
+  );
+};
+
+//Componente para el total
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
