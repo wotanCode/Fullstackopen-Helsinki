aa99d5d43a0c5df70cb8723be82b69fa25861714
Pedro Yanez <pedro_y91@outlook.com>
2021-10-11 21:18:35 -0300

    iniciando ejercicio 2.1

--- Parte 02/2.1-2.5/src/index.js
+++ Parte 02/2.1-2.5/src/index.js
@@ -1,9 +1,3 @@
-/*
-Basado en la seccion
-https://fullstackopen.com/es/part1/introduccion_a_react
-https://fullstackopen.com/es/part1/java_script
-*/
-
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -50,32 +44,42 @@ const Total = (props) => {
   );
 };
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
+const Course = (props) => {
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
     </div>
   );
 };
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
