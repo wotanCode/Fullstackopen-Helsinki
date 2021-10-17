851044bc0f4118bfadaa3de654d74dc68db98837
Pedro Yanez <pedro_y91@outlook.com>
2021-10-17 12:42:00 -0300

    2.1 listo

--- Parte 02/2.1-2.5/src/index.js
+++ Parte 02/2.1-2.5/src/index.js
@@ -49,37 +49,35 @@ const Course = (props) => {
     <div>
       <Header course={props.course.name} />
       <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
+      {/* <Total parts={props.course.parts} /> */}
     </div>
   );
 };
 
-
-
 const App = () => {
   const course = {
-    id: 1,
-    name: 'Half Stack application development',
+    //id: 1,
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
         id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
         id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
         id: 3,
       },
     ],
-  }
+  };
 
-  return <Course course={course} />
-}
+  return <Course course={course} />;
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
